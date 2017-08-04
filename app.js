9735cb56db031d4679f45a554db5427bcdbeca5b
thao.pham <thaopv@funix.edu.vn>
2017-08-04 20:57:53 +0700

    crud 04/08/2017

--- app.js
+++ app.js
@@ -15,61 +15,73 @@ app.use('/profilepics', express.static('images'));
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.get('/',(req,res)=>{
-    let users = [];
-    fs.readdir('users',function(err, files){
-        if(err) throw err
-        files.forEach(function(file){
-            fs.readFile(path.join(__dirname,'users',file),{encoding: 'utf8'},function(err, data){
-                 if (err) throw err
-                 const user = JSON.parse(data);
-                 user.name.full = _.startCase(user.name.first + ' ' + user.name.last);
-                 users.push(user);
-                 if(users.length === files.length)
-                    //res.send(users);
-                 res.render('index', {users: users})
-            } )
-        })
-    })
-})
 
-app.get('/:username', function (req, res) {
-  var username = req.params.username
-  var user = getUser(username)
-  //res.send(user);
-  
-  res.render('user', {
-    user: user,
-    address: user.location
-  })
-})
-
-app.put('/:username',(req,res)=>{
-    var username = req.params.username
-    var user = getUser(username)
-    saveUser(username, req.body)
-    res.send(req.body)
-})
-
-function getUser(userName)
-{
-    var user = JSON.parse(fs.readFileSync(getUserFilePath(userName),{encoding:'utf8'}))
-    user.name.full = _.startCase(user.name.first + ' ' + user.name.last)
-    _.keys(user.location).forEach(function (key) {
-        user.location[key] = _.startCase(user.location[key])
-    })
-    return user
+function getUserFilePath (username) {
+  return path.join(__dirname, 'users', username) + '.json'
 }
+
+function getUser (username) {
+  var user = JSON.parse(fs.readFileSync(getUserFilePath(username), {encoding: 'utf8'}))
+  user.name.full = _.startCase(user.name.first + ' ' + user.name.last)
+  _.keys(user.location).forEach(function (key) {
+    user.location[key] = _.startCase(user.location[key])
+  })
+  return user
+}
+
 function saveUser (username, data) {
   var fp = getUserFilePath(username)
   fs.unlinkSync(fp) // delete the file
   fs.writeFileSync(fp, JSON.stringify(data, null, 2), {encoding: 'utf8'})
 }
 
-function getUserFilePath(userName){
-    return path.join(__dirname, 'users', userName) + '.json';
-}
+app.engine('hbs', engines.handlebars)
 
+app.set('views', './views')
+app.set('view engine', 'hbs')
 
+app.use('/profilepics', express.static('images'))
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.get('/favicon.ico', function (req, res) {
+  res.end()
+})
+
+app.get('/', function (req, res) {
+  var users = []
+  fs.readdir('users', function (err, files) {
+    files.forEach(function (file) {
+      fs.readFile(path.join(__dirname, 'users', file), {encoding: 'utf8'}, function (err, data) {
+        var user = JSON.parse(data)
+        user.name.full = _.startCase(user.name.first + ' ' + user.name.last)
+        users.push(user)
+        if (users.length === files.length) res.render('index', {users: users})
+      })
+    })
+  })
+})
+
+app.get('/:username', function (req, res) {
+  var username = req.params.username
+  var user = getUser(username)
+  res.render('user', {
+    user: user,
+    address: user.location
+  })
+})
+
+app.put('/:username', function (req, res) {
+  var username = req.params.username
+  var user = getUser(username)
+  user.location = req.body
+  saveUser(username, user)
+  res.end()
+})
+
+app.delete('/:username', function (req, res) {
+  var fp = getUserFilePath(req.params.username)
+  fs.unlinkSync(fp) // delete the file
+  res.sendStatus(200)
+})
 
 app.listen(3000,()=>{console.log("App running at port 3000");})
